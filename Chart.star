b344dc4892ed9485dacf7d062590818cae5d1c0b
Ulrich Kramer <u.kramer@sap.com>
2020-02-16 08:42:00 +0100

    Convert cf-for-k8s to shalm
    Fix copy-paste error for metric-proxy cert.
    
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>
    
    Set fixed name for shalm chart
    
    Use defaults where possible
    
    Move domain logic to cf-for-k8s-gardener
    
    Add app_domain_prefix parameter
    
    Add app_domain_prefix parameter
    
    Add app_domains parameter
    
    Implement lazy evaluation of overlays
    
    Add fixes for kpack
    
    CKI-852 Support configuration of Buildpacks
    
    CKI-852 Support configuration of Buildpacks
    
    CKI-852 Support configuration of Buildpacks
    
    Fix log_cache_client
    
    Fix cloud controller handling of VCAP_SERVICES
    
    Move fixes from cf-for-k8s to cf-for-k8s-gardener
    
    Rename capi-api-server to cf-api-server

---
+++ Chart.star
@@ -0,0 +1,54 @@
+def init(self,domain=None,ytt_files=[],docker_registry=None,app_domains = None):
+  self.domain = domain
+  self.app_domains = app_domains
+  self.__class__.name = "cf-for-k8s"
+  self.ytt_files = ytt_files
+  self.docker_registry = docker_registry
+  self.cf_admin_password = user_credential("cf-admin-password-shalm",username="admin")
+  self.secret_key = user_credential("secret-key-shalm",username="admin")
+  self.cf_db_admin_password = user_credential("cf-db-admin-password-shalm",username="admin")
+  self.capi_database_password = user_credential("capi-database-password-shalm",username="admin")
+  self.ca = certificate("ca-shalm",is_ca=True)
+  self.system_certificate = certificate("system-certificate-shalm",signer=self.ca,domains=["*.cf-system.svc.cluster.local" ])
+  self.log_cache_ca = certificate("log-cache-ca-shalm",is_ca=True)
+  self.log_cache = certificate("log-cache-shalm",signer=self.log_cache_ca,domains=["log-cache"])
+  self.log_cache_metrics = certificate("log-cache-metrics-shalm",signer=self.log_cache_ca,domains=["log-cache-metrics"])
+  self.log_cache_gateway = certificate("log-cache-gateway-shalm",signer=self.log_cache_ca,domains=["log-cache-gateway","localhost"])
+  self.log_cache_syslog = certificate("log-cache-syslog-shalm",signer=self.log_cache_ca,domains=["log-cache-syslog"])
+  self.metric_proxy_ca = certificate("metrics-proxy-ca-shalm",is_ca=True)
+  self.metric_proxy = certificate("metrics-proxy-shalm",signer=self.metric_proxy_ca,domains=["metric-proxy"])
+
+  
+  self.uaa_db_password= user_credential("uaa-db-password-shalm",username="admin")
+  self.uaa_admin_client_secret= user_credential("uaa-admin-client-secret-shalm",username="admin")
+  self.uaa_jwt_policy_signing_key = certificate("uaa-jwt-policy-signing-key-shalm",signer=self.ca,domains=["uaa-jwt-policy-signing-key"])
+  self.uaa_login_service_provider = certificate("uaa-login-service-provider-shalm",signer=self.ca,domains=["uaa-login-service-provider"])
+  self.uaa_encryption_key_passphrase= user_credential("uaa-encryption-key-passphrase-shalm",username="admin")
+  self.docker_registry_http_secret= user_credential("docker-registry-http-secret-shalm",username="admin")
+
+def template(self,glob=""):
+  ytt_files = self.ytt_files
+  if type(ytt_files)  == "function" or type(ytt_files)  == "builtin_function_or_method"  :
+    ytt_files = ytt_files()
+  return self.ytt("config",self.helm("templates",glob="values.yaml"),*ytt_files)
+
+
+def apply(self,k8s):
+  if not self.domain:
+    fail("Mandatory parameter domain not set. Use --set domain=... to pass this parameter.")
+  k8s.tool = "kapp"
+  self.__apply(k8s)
+  k8s.rollout_status("deployment","cf-api-server",namespace='cf-system')
+
+
+def delete(self,k8s):
+  if not self.domain:
+    fail("Mandatory parameter domain not set. Use --set domain=... to pass this parameter.")
+  k8s.tool = "kapp"
+  self.__delete(k8s)
+
+def credentials(self):
+  return struct(username=self.cf_admin_password.username, password=self.cf_admin_password.password,url="https://api." + self.domain)
+
+def uaa_credentials(self):
+  return struct(url="https://uaa." + self.domain,client_secret=self.uaa_admin_client_secret.password, client_id=self.uaa_admin_client_secret.username)
--- hack/generate-values.sh
+++ hack/generate-values.sh
@@ -78,137 +78,136 @@ bosh --version >/dev/null
 bosh interpolate --vars-store=${VARS_FILE} <(
   cat <<EOF
 variables:
-- name: cf_admin_password
-  type: password
-- name: blobstore_secret_key
-  type: password
-- name: db_admin_password
-  type: password
-- name: capi_db_password
-  type: password
-- name: uaa_db_password
-  type: password
-- name: uaa_admin_client_secret
-  type: password
-- name: uaa_encryption_key_passphrase
-  type: password
-- name: default_ca
-  type: certificate
-  options:
-    is_ca: true
-    common_name: ca
-- name: system_certificate
-  type: certificate
-  options:
-    ca: default_ca
-    common_name: "*.${DOMAIN}"
-    alternative_names:
-    - "*.${DOMAIN}"
-    - "*.login.${DOMAIN}"
-    - "*.uaa.${DOMAIN}"
-    - "*.apps.${DOMAIN}"
-    - "*.cf-system.svc.cluster.local"
-    extended_key_usage:
-    - client_auth
-    - server_auth
+# - name: cf_admin_password
+#   type: password
+# - name: blobstore_secret_key
+#   type: password
+# - name: db_admin_password
+#   type: password
+# - name: capi_db_password
+#   type: password
+# - name: uaa_db_password
+#   type: password
+# - name: uaa_admin_client_secret
+#   type: password
+# - name: uaa_encryption_key_passphrase
+#   type: password
+# - name: default_ca
+#   type: certificate
+#   options:
+#     is_ca: true
+#     common_name: ca
+# - name: system_certificate
+#   type: certificate
+#   options:
+#     ca: default_ca
+#     common_name: "*.${DOMAIN}"
+#     alternative_names:
+#     - "*.${DOMAIN}"
+#     - "*.login.${DOMAIN}"
+#     - "*.uaa.${DOMAIN}"
+#     - "*.apps.${DOMAIN}"
+#     - "*.cf-system.svc.cluster.local"
+#     extended_key_usage:
+#     - client_auth
+#     - server_auth
 
 - name: uaa_jwt_policy_signing_key
-  type: certificate
-  options:
-    ca: default_ca
-    common_name: uaa_jwt_policy_signing_key
+#   type: certificate
+#   options:
+#     ca: default_ca
+#     common_name: uaa_jwt_policy_signing_key
 
 - name: uaa_login_service_provider
-  type: certificate
-  options:
-    ca: default_ca
-    common_name: uaa_login_service_provider
+#   type: certificate
+#   options:
+#     ca: default_ca
+#     common_name: uaa_login_service_provider
 
 - name: log_cache_ca
-  type: certificate
-  options:
-    is_ca: true
-    common_name: log-cache-ca
+#   type: certificate
+#   options:
+#     is_ca: true
+#     common_name: log-cache-ca
 
 - name: log_cache
-  type: certificate
-  options:
-    ca: log_cache_ca
-    common_name: log-cache
-    extended_key_usage:
-    - client_auth
-    - server_auth
-
+#   type: certificate
+#   options:
+#     ca: log_cache_ca
+#     common_name: log-cache
+#     extended_key_usage:
+#     - client_auth
+#     - server_auth
 - name: log_cache_syslog
-  type: certificate
-  options:
-    ca: log_cache_ca
-    common_name: log-cache-syslog
-    extended_key_usage:
-    - client_auth
-    - server_auth
+#   type: certificate
+#   options:
+#     ca: log_cache_ca
+#     common_name: log-cache-syslog
+#     extended_key_usage:
+#     - client_auth
+#     - server_auth
 
 - name: log_cache_metrics
-  type: certificate
-  options:
-    ca: log_cache_ca
-    common_name: log-cache-metrics
-    extended_key_usage:
-    - client_auth
-    - server_auth
+#   type: certificate
+#   options:
+#     ca: log_cache_ca
+#     common_name: log-cache-metrics
+#     extended_key_usage:
+#     - client_auth
+#     - server_auth
 
 - name: log_cache_gateway
-  type: certificate
-  options:
-    ca: log_cache_ca
-    common_name: log-cache-gateway
-    alternative_names:
-    - localhost
-    extended_key_usage:
-    - client_auth
-    - server_auth
+#  type: certificate
+#  options:
+#    ca: log_cache_ca
+#    common_name: log-cache-gateway
+#    alternative_names:
+#    - localhost
+#    extended_key_usage:
+#    - client_auth
+#    - server_auth
 
 - name: metric_proxy_ca
-  type: certificate
-  options:
-    is_ca: true
-    common_name: metric-proxy-ca
+#  type: certificate
+#  options:
+#    is_ca: true
+#    common_name: metric-proxy-ca
 
 - name: metric_proxy
-  type: certificate
-  options:
-    ca: metric_proxy_ca
-    common_name: metric-proxy
-    extended_key_usage:
-    - client_auth
-    - server_auth
+#  type: certificate
+#  options:
+#    ca: metric_proxy_ca
+#    common_name: metric-proxy
+#    extended_key_usage:
+#    - client_auth
+#    - server_auth
 EOF
 ) >/dev/null
 
 cat <<EOF
-#@data/values
----
-system_domain: "${DOMAIN}"
-app_domains:
-#@overlay/append
-- "apps.${DOMAIN}"
-cf_admin_password: $(bosh interpolate ${VARS_FILE} --path=/cf_admin_password)
+@data/values
+ ---
+# system_domain: "${DOMAIN}"
+# app_domains:
+# #@overlay/append
+# - "${DOMAIN}"
+# cf_admin_password: $( bosh interpolate ${VARS_FILE} --path=/cf_admin_password )
 
 cf_blobstore:
-  secret_key: $(bosh interpolate ${VARS_FILE} --path=/blobstore_secret_key)
+#   secret_key: $( bosh interpolate ${VARS_FILE} --path=/blobstore_secret_key )
 
 cf_db:
-  admin_password: $(bosh interpolate ${VARS_FILE} --path=/db_admin_password)
+#   admin_password: $( bosh interpolate ${VARS_FILE} --path=/db_admin_password )
 
 capi:
-  database:
-    password: $(bosh interpolate ${VARS_FILE} --path=/capi_db_password)
+#   database:
+#     password: $( bosh interpolate ${VARS_FILE} --path=/capi_db_password )
 
 system_certificate:
-  #! This certificates and keys are base64 encoded and should be valid for *.system.cf.example.com
-  crt: &crt $(bosh interpolate ${VARS_FILE} --path=/system_certificate/certificate | base64 | tr -d '\n')
-  key: &key $(bosh interpolate ${VARS_FILE} --path=/system_certificate/private_key | base64 | tr -d '\n')
-  ca: $(bosh interpolate ${VARS_FILE} --path=/system_certificate/ca | base64 | tr -d '\n')
+#   #! This certificates and keys are base64 encoded and should be valid for *.system.cf.example.com
+#   crt: &crt $( bosh interpolate ${VARS_FILE} --path=/system_certificate/certificate | base64 | tr -d '\n' )
+#   key: &key $( bosh interpolate ${VARS_FILE} --path=/system_certificate/private_key | base64 | tr -d '\n' )
+#   ca: $( bosh interpolate ${VARS_FILE} --path=/system_certificate/ca | base64 | tr -d '\n' )
 
 workloads_certificate:
   crt: *crt
@@ -216,62 +215,62 @@ workloads_certificate:
   ca: $(bosh interpolate ${VARS_FILE} --path=/system_certificate/ca | base64 | tr -d '\n')
 
 log_cache_ca:
-  crt: $(bosh interpolate ${VARS_FILE} --path=/log_cache_ca/certificate | base64 | tr -d '\n')
-  key: $(bosh interpolate ${VARS_FILE} --path=/log_cache_ca/private_key | base64 | tr -d '\n')
+#   crt: $( bosh interpolate ${VARS_FILE} --path=/log_cache_ca/certificate | base64 | tr -d '\n' )
+#   key: $( bosh interpolate ${VARS_FILE} --path=/log_cache_ca/private_key | base64 | tr -d '\n' )
 
 log_cache:
-  crt: $(bosh interpolate ${VARS_FILE} --path=/log_cache/certificate | base64 | tr -d '\n')
-  key: $(bosh interpolate ${VARS_FILE} --path=/log_cache/private_key | base64 | tr -d '\n')
+#   crt: $( bosh interpolate ${VARS_FILE} --path=/log_cache/certificate | base64 | tr -d '\n' )
+#   key: $( bosh interpolate ${VARS_FILE} --path=/log_cache/private_key | base64 | tr -d '\n' )
 
 log_cache_metrics:
-  crt: $(bosh interpolate ${VARS_FILE} --path=/log_cache_metrics/certificate | base64 | tr -d '\n')
-  key: $(bosh interpolate ${VARS_FILE} --path=/log_cache_metrics/private_key | base64 | tr -d '\n')
+#   crt: $( bosh interpolate ${VARS_FILE} --path=/log_cache_metrics/certificate | base64 | tr -d '\n' )
+#   key: $( bosh interpolate ${VARS_FILE} --path=/log_cache_metrics/private_key | base64 | tr -d '\n' )
 
 log_cache_gateway:
-  crt: $(bosh interpolate ${VARS_FILE} --path=/log_cache_gateway/certificate | base64 | tr -d '\n')
-  key: $(bosh interpolate ${VARS_FILE} --path=/log_cache_gateway/private_key | base64 | tr -d '\n')
+#   crt: $( bosh interpolate ${VARS_FILE} --path=/log_cache_gateway/certificate | base64 | tr -d '\n' )
+#   key: $( bosh interpolate ${VARS_FILE} --path=/log_cache_gateway/private_key | base64 | tr -d '\n' )
 
 log_cache_syslog:
-  crt: $(bosh interpolate ${VARS_FILE} --path=/log_cache_syslog/certificate | base64 | tr -d '\n')
-  key: $(bosh interpolate ${VARS_FILE} --path=/log_cache_syslog/private_key | base64 | tr -d '\n')
+#   crt: $( bosh interpolate ${VARS_FILE} --path=/log_cache_syslog/certificate | base64 | tr -d '\n' )
+#   key: $( bosh interpolate ${VARS_FILE} --path=/log_cache_syslog/private_key | base64 | tr -d '\n' )
 
 metric_proxy:
-  ca:
-    crt: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy_ca/certificate | base64 | tr -d '\n' )
-    key: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy_ca/private_key | base64 | tr -d '\n' )
-  cert:
-    crt: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy/certificate | base64 | tr -d '\n' )
-    key: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy/private_key | base64 | tr -d '\n' )
+#  ca:
+#    crt: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy_ca/certificate | base64 | tr -d '\n' )
+#    key: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy_ca/private_key | base64 | tr -d '\n' )
+#  cert:
+#    crt: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy/certificate | base64 | tr -d '\n' )
+#    key: $( bosh interpolate ${VARS_FILE} --path=/metric_proxy/private_key | base64 | tr -d '\n' )
 
 uaa:
-  database:
-    password: $(bosh interpolate ${VARS_FILE} --path=/uaa_db_password)
-  admin_client_secret: $(bosh interpolate ${VARS_FILE} --path=/uaa_admin_client_secret)
-  certificate:
-    crt: *crt
-    key: *key
-  jwt_policy:
-    signing_key: |
-$(bosh interpolate "${VARS_FILE}" --path=/uaa_jwt_policy_signing_key/private_key | sed -e 's#^#      #')
-  encryption_key:
-    passphrase: $(bosh interpolate "${VARS_FILE}" --path=/uaa_encryption_key_passphrase)
-  login:
-    service_provider:
-      key: |
-$(bosh interpolate "${VARS_FILE}" --path=/uaa_login_service_provider/private_key | sed -e 's#^#        #')
-      certificate: |
-$(bosh interpolate "${VARS_FILE}" --path=/uaa_login_service_provider/certificate | sed -e 's#^#        #')
+#   database:
+#     password: $( bosh interpolate ${VARS_FILE} --path=/uaa_db_password )
+#   admin_client_secret: $( bosh interpolate ${VARS_FILE} --path=/uaa_admin_client_secret )
+#   certificate:
+#     crt: *crt
+#     key: *key
+#   jwt_policy:
+#     signing_key: |
+# $( bosh interpolate "${VARS_FILE}" --path=/uaa_jwt_policy_signing_key/private_key | sed -e 's#^#      #' )
+#   encryption_key:
+#     passphrase: $( bosh interpolate "${VARS_FILE}" --path=/uaa_encryption_key_passphrase )
+#   login:
+#     service_provider:
+#       key: |
+# $( bosh interpolate "${VARS_FILE}" --path=/uaa_login_service_provider/private_key | sed -e 's#^#        #' )
+#       certificate: |
+# $( bosh interpolate "${VARS_FILE}" --path=/uaa_login_service_provider/certificate | sed -e 's#^#        #' )
 
 doppler:
-  tls:
-    crt: *crt
-    key: *key
+#   tls:
+#     crt: *crt
+#     key: *key
 
 eirini:
-  tls:
-    crt: *crt
-    key: *key
-EOF
+#   tls:
+#     crt: *crt
+#     key: *key
+# EOF
 
 if [[ -n "${GCP_SERVICE_ACCOUNT_JSON:=}" ]]; then
   hostname="gcr.io"
@@ -283,11 +282,11 @@ if [[ -n "${GCP_SERVICE_ACCOUNT_JSON:=}" ]]; then
   cat <<EOF
 
 app_registry:
-  hostname: gcr.io
-  repository: gcr.io/$( bosh interpolate ${GCP_SERVICE_ACCOUNT_JSON} --path=/project_id )/cf-workloads
-  username: _json_key
-  password: |
-$(cat ${GCP_SERVICE_ACCOUNT_JSON} | sed -e 's/^/    /')
+#   hostname: gcr.io
+#   repository: gcr.io/$( bosh interpolate ${GCP_SERVICE_ACCOUNT_JSON} --path=/project_id )/cf-workloads
+#   username: _json_key
+#   password: |
+# $( cat ${GCP_SERVICE_ACCOUNT_JSON} | sed -e 's/^/    /' )
 EOF
 
 fi
